b2277d915d1eae30b4cf5dd285de436494dcbf03
ainerosinski <ainerosinski@gmail.com>
2025-03-20 02:27:46 +0000

    Add 14.py

---
+++ src/14.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+print(get_random_string(10))
