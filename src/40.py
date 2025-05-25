500f60957df0aeb55d91c4c4adcf79bc24159ca6
ainerosinski <ainerosinski@gmail.com>
2025-05-25 23:07:58 +0000

    Add 40.py

---
+++ src/40.py
@@ -0,0 +1,7 @@
+def calculate_total_expenses(expenses):
+    total = sum(expenses)
+    return total
+
+expenses = [100.50, 200.78, 300.45]
+total_expense = calculate_total_expenses(expenses)
+print("The total expenses are:", total_expense)
